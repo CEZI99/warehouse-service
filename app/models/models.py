6058ec93a0e3ddaf8684f6737408d037ecba482a
CEZI <restler38restler@gmail.com>
2025-07-22 14:32:22 +0300

    init

---
+++ app/models/models.py
@@ -0,0 +1,47 @@
+"""models.py"""
+from datetime import datetime
+from pydantic import BaseModel
+from typing import Optional
+
+class KafkaEvent(BaseModel):
+    id: str
+    source: str
+    specversion: str
+    type: str
+    datacontenttype: str
+    dataschema: str
+    time: int
+    subject: str
+    destination: str
+    data: 'EventData'
+
+class EventData(BaseModel):
+    movement_id: str
+    warehouse_id: str
+    timestamp: str
+    event: str  # "arrival" или "departure"
+    product_id: str
+    quantity: int
+
+class Movement(BaseModel):
+    id: str
+    movement_type: str  # "arrival" или "departure"
+    warehouse_id: str
+    product_id: str
+    quantity: int
+    timestamp: datetime
+    related_movement_id: Optional[str] = None  # для связи отправки и приемки
+    
+    class Config:
+        orm_mode = True
+
+class WarehouseStock(BaseModel):
+    warehouse_id: str
+    product_id: str
+    quantity: int
+    last_updated: datetime
+    
+    class Config:
+        orm_mode = True
+
+KafkaEvent.update_forward_refs()
